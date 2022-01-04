f32777105a6b4a0b10d3af3956329858a21e1cec
Gustavo Armando Tovar Cordero <66337926+Gatc-18@users.noreply.github.com>
2022-01-04 17:08:10 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,9 @@ let template = document.getElementById('template-card').content;
 let fragment = document.createDocumentFragment();
 let itmMarvel = document.getElementById('itemMarvel');
 let itmDc = document.getElementById('itemDC');
+let contenedor = document.getElementById('pruebaDelModal');
+
+
 
 btnMarvel.addEventListener('click', () => {
 
@@ -23,9 +26,11 @@ btnMarvel.addEventListener('click', () => {
         template.querySelector('h4').textContent = publisher;
         template.querySelector('img').setAttribute('src', image );
         template.querySelector('img').dataset.id = id;
+        template.querySelector('button').setAttribute('id', id)
 
         const clone = template.cloneNode(true);
         fragment.appendChild(clone)
+        
     })
     itmMarvel.appendChild(fragment)
 })
@@ -44,11 +49,71 @@ btnDc.addEventListener('click', () => {
         template.querySelector('h5').textContent = superhero;
         template.querySelector('h4').textContent = publisher;
         template.querySelector('img').setAttribute('src', image );
-        template.querySelector('img').dataset.id = id;
-
+        // template.querySelector('img').dataset.id = superhero;
+        template.querySelector('button').setAttribute('id', id);
         const clone = template.cloneNode(true);
         fragment.appendChild(clone)
     })
-    itmDc.appendChild(fragment)
+    itmDc.appendChild(fragment);
 })
+
+itmDc.addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn')){
+
+        let idHeroe = e.srcElement.id;
+
+        data.forEach(item => {
+               const { id, superhero, publisher, image} = item;
+
+               if (idHeroe == id ){
+                   contenedor.innerHTML = `
+                   <div class="modal-header">
+                   <h5 class="modal-title" id="exampleModalLabel">${superhero}</h5>
+                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+               </div>
+               <div class="modal-body">
+                   <center>
+                       <img src="${image}" alt="">
+                   </center>
+                   <h3>Franquicia: ${publisher}</h3>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda natus, aperiam architecto
+                       tempora unde soluta ducimus ea, ipsum cumque dicta ab laboriosam accusantium nemo itaque
+                       provident voluptatibus dolorum laborum ratione.</p>
+               </div>
+                   `  
+               }
+        })
+     
+    }
+})
+
+itmMarvel.addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn')){
+
+        let idHeroe = e.srcElement.id;
+
+        data.forEach(item => {
+               const { id, superhero, publisher, image, first_appearance} = item;
+
+               if (idHeroe == id ){
+                   contenedor.innerHTML = `
+                   <div class="modal-header">
+                   <h5 class="modal-title" id="exampleModalLabel">${superhero}</h5>
+                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+               </div>
+               <div class="modal-body">
+                   <center>
+                       <img src="${image}" alt="">
+                   </center>
+                   <h3>Franquicia: ${publisher}</h3>
+                   <p>${superhero} es un gran heroe que tuvo su primera aparicion en el comic ${first_appearance}</p>
+               </div>
+                   `  
+               }
+        })
+     
+    }
+})
+
+
 
